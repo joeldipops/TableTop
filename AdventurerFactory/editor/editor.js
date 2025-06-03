4a9013327e9c1ae1b5cd7be35201fa621da0134b
joeldipops@gmail.com <joeldipops@gmail.com>
2025-06-03 17:48:50 +1000

    Filters rendering

--- AdventurerFactory/editor/editor.js
+++ AdventurerFactory/editor/editor.js
@@ -1,11 +1,30 @@
 // Main logic of the application.
 (async function() {
+    const currentFilters = {};
     const onAddClick = function(event) {
         alert(`Clicked: ${event.currentTarget.getAttribute("data-cardid")}`);
     };
 
+    const onFilterChange = function(event) {
+        document.querySelectorAll("#filters select")
+        .forEach(function(el) {
+            const name = el.getAttribute("name");
+            if (el.value === "Any") {
+                delete el[name];
+            } else {
+                filters[el.getAttribute("name")] = el.value;
+            }
+        });
+
+        let displayList = filterList(_fullList);
+        displayList = sortList(displayList);
+
+        renderList(displayList, _cardTemplate);
+    }
+
     const events = [
-        ["click", "add", onAddClick]
+        ["click", "add", onAddClick],
+        ["change", "types", onFilterChange]
     ];
 
     const santiseForFilename = function(name) {
@@ -15,7 +34,11 @@
     const loadCardList = async function() {
         return await sendHttpRequest("/cards.json", "GET")
         .then(function(response) {
-            return JSON.parse(response.responseText).cards;
+            const cards = JSON.parse(response.responseText).cards;
+            cards.forEach(function(card) {
+                card.identity = santiseForFilename(card.name);
+            });
+            return cards;
         });
     };
 
@@ -26,8 +49,8 @@
         });
     };
 
-    const filterList = function() {
-        return _fullList;
+    const filterList = function(list) {
+        return list;
     };
 
     const sortList = function(list) {
@@ -37,7 +60,6 @@
     const renderList = function(list, template) {
         const container = document.getElementById("filteredList");
         list.forEach(function(card) {
-            card.identity = santiseForFilename(card.name);
             let html;
 
             for(let key in card) {
@@ -49,6 +71,79 @@
             }
 
             container.insertAdjacentHTML("beforeend", html);
+        });
+    };
+
+    const getAllFills = function(list) {
+        return list.reduce(function(result, card) {
+            card.fills.map(function(fill) {
+                return fill.key;
+            })
+            .forEach(function(fill) {
+                // Artifact of the card generation process.
+                // TODO ... yeah.
+                if (["slash", "and"].includes(fill)) {
+                    return;
+                }
+                // This only needs to appear once.
+                if (["C1", "C2", "C3", "C4"].includes(fill)) {
+                    fill = "Class";
+                }
+                if (!result.includes(fill)) {
+                    result.push(fill);
+                }
+            });
+            return result;
+        }, []);
+    }
+
+    const getAllNeeds = function(list) {
+        return list.reduce(function(result, card) {
+            card.needs.map(function(need) {
+                return need.key;
+            })
+            .forEach(function(need) {
+                if (!result.includes(need)) {
+                    result.push(need);
+                }
+            });
+            return result;
+        }, []);
+    }
+
+    const getAllTypes = function(list) {
+        return list.reduce(function(result, card) {
+            const types = (card.types || "").split("/");
+            types.forEach(function(type) {
+                type = type.trim();
+                if (!result.includes(type)) {
+                    result.push(type);
+                }
+            });
+            return result;
+        }, []);
+    };
+
+    const getFilterValues = function(list, name) {
+        switch(name) {
+            case "types": return getAllTypes(list);
+            case "fills": return getAllFills(list);
+            case "needs": return getAllNeeds(list);
+            default: throw new Error("What is this filter?", name);
+        }
+    };
+
+    const renderFilters = function(list) {
+        document.querySelectorAll("#filters select")
+        .forEach(function(el) {
+            const name = el.getAttribute("name");
+            getFilterValues(list, name)
+            .forEach(function(val) {
+                const opt = document.createElement("option");
+                opt.value = val;
+                opt.innerText = val;
+                el.appendChild(opt);
+            });
         });
     };
 
@@ -71,6 +166,8 @@
     const response = await Promise.all([loadCardList(), loadCardTemplate()]);
     const _fullList = response[0];
     const _cardTemplate = response[1];
+
+    renderFilters(_fullList);
 
     let displayList = filterList(_fullList);
     displayList = sortList(displayList);
